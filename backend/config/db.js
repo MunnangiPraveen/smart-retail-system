1dbe2a4bdefd0c5ae0ba93a8d87d027a072497d2
PraveenMunnangi <munnangipr@gmail.com>
2026-05-15 03:46:13 +0530

    fixed mysql callback issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,10 +5,7 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    port: process.env.DB_PORT
 });
 
-module.exports = db.promise();
+module.exports = db;
