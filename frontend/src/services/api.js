c3e93e05aeab681b9615b70e318b9f975c408f7b
PraveenMunnangi <munnangipr@gmail.com>
2026-05-15 01:49:52 +0530

    connected frontend with deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-
-    baseURL:
-        "http://localhost:5000/api"
-
+    baseURL: "https://smart-retail-backend-dbpz.onrender.com/api"
 });
 
 export default API;
