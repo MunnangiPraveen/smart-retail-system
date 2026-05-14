af1cd8cfbbcb85c6ccf6d1309d13404922191837
PraveenMunnangi <munnangipr@gmail.com>
2026-05-15 03:06:23 +0530

    fixed railway db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,29 +1,19 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-
-    host: "localhost",
-
-    user: "root",
-
-    password: "praveen@2005",
-
-    database: "smart_retail"
-
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
-connection.connect((err) => {
-
+db.connect((err) => {
     if (err) {
-
-        console.log(err);
-
+        console.log("Database Connection Failed:", err);
     } else {
-
         console.log("MySQL Connected");
-
     }
-
 });
 
-module.exports = connection;
+module.exports = db;
